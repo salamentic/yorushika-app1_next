8cb5d65b16365bd85fb7d22e10078dace947404e
salamentic <raniruddh99@gmail.com>
2025-10-20 22:39:34 -0400

    Added localization for footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,17 +1,19 @@
+import {useTranslations} from 'next-intl';
 
 function Footer() {
+  const t = useTranslations('Footer');
   return (
     <footer className="w-full py-5 text-white shadow-md" style={{ backgroundColor: '#499FD8' }}>
       <div className="container flex flex-col items-center justify-center px-4 mx-auto">
         <p className="text-xl font-bold" style={{fontFamily: 'var(--font-kaisei), serif'}}>
-          月猫図書館
+	  {t('title')}
         </p>
         <p className="mt-3 text-xs sm:text-sm text-center">
-          ※ 本サイトはファンによる個人運営であり、公式とは一切関係ありません
+	  {t('fan-site-notice')}
         </p>
         <div className="mt-1 text-xs sm:text-sm space-x-4">
           <a href="/privacy" className="hover:text-blue-100 underline transition-colors">
-            プライバシーポリシー
+	  {t('privacy-policy')}
           </a>
         </div>
       </div>
